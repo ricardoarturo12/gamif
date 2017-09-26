72891aa7318c7aa85d1aff0521321ce2a9a9c4bc
Ricardo Molinas <ricardo121990@gmail.com>
2017-09-26 13:56:46 +0000

    Primer commit

---
+++ gamification/post/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+	author = mod
